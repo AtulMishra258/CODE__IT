152aea900d0ca7c1f3b5d7a6a50b45c2ba34d788
Atul Kumar Mishra <101014298+AtulMishra258@users.noreply.github.com>
2023-03-15 08:27:39 +0530

    Update Exercise_1.java

--- Code_IT/src/main/java/Exercise_1.java
+++ Code_IT/src/main/java/Exercise_1.java
@@ -17,26 +17,6 @@ public class Exercise_1 {
                 System.out.print(i+" ");
             }
         }
-//        Using Recursion
-          print(1);
     }
-    static void print(int num){
-        if(num == 100){
-            System.out.println("Buzz ");
-            return;
-        }
-         if(num%3 == 0 && num%5 == 0){
-                System.out.print("FizzBuzz ");
-            }
-            else if(num%3 == 0){
-                System.out.print("Fizz ");
-            }
-            else if(num%5 == 0){
-                System.out.print("Buzz ");
-            }
-            else{
-                System.out.print(num+" ");
-            }
-         print(num+1);        
-    }
+  
 }
